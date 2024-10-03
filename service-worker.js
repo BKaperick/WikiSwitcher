75f03c7c44134e36575867ae67fc8864e0a094d7
Bryan Kaperick <bryankaperick@gmail.com>
2024-10-03 16:12:25 +0200

    service worker now creates a new tab at youtube on page load

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.create({url: "https://www.youtube.com"});
-});
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete' && tab.active) {
+      chrome.tabs.create({url: "https://www.youtube.com"});
+  }
+})
