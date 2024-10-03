557d7c03cbcc2ec98d3143e41a0bbff0aa43651f
Bryan Kaperick <bryankaperick@gmail.com>
2024-10-03 16:20:55 +0200

    permissions issue with chrome.tabs api

--- popup.js
+++ popup.js
@@ -8,13 +8,3 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 // window.onload = onWindowLoad;
 
-function DOMtoString(selector) {
-    console.log("DOMtoString");
-    if (selector) {
-        selector = document.querySelector(selector);
-        if (!selector) return "ERROR: querySelector failed to find node"
-    } else {
-        selector = document.documentElement;
-    }
-    return selector.outerHTML;
-};
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,32 @@
+function DOMtoString(selector) {
+    console.log("DOMtoString");
+    if (selector) {
+        selector = document.querySelector(selector);
+        if (!selector) return "ERROR: querySelector failed to find node"
+    } else {
+        selector = document.documentElement;
+    }
+    return selector.outerHTML;
+};
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-  if (changeInfo.status == 'complete' && tab.active) {
-      chrome.tabs.create({url: "https://www.youtube.com"});
-  }
+//  if (changeInfo.status == 'complete' && tab.active) {
+      // chrome.tabs.create({url: "https://www.youtube.com"});
+    console.log("doing shit");
+    chrome.tabs.query({}, (tabs) => {
+            var activeTab = tabs[0];
+            var activeTabId = activeTab.id;
+
+            return chrome.scripting.executeScript({
+                target: { tabId: activeTabId },
+                // injectImmediately: true,  // uncomment this to make it execute straight away, other wise it will wait for document_idle
+                func: DOMtoString,
+                // args: ['body']  // you can use this to target what element to get the html for
+            });
+
+        }).then(function (results) {
+            message.innerText = results[0].result;
+        }).catch(function (error) {
+            message.innerText = 'There was an error injecting script : \n' + error.message;
+        });
+//  }
 })
