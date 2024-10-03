a9ac0e748a0eaca129d22f283a2a62298f374701
Bryan Kaperick <bryankaperick@gmail.com>
2024-10-03 16:06:11 +0200

    no errors, but on click extension button shows empty popup

--- popup.js
+++ popup.js
@@ -1,12 +1,12 @@
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
- sendResponse({dom: document.body.innerHTML});
-});
-
-window.onload = onWindowLoad;
-
 function sendResponse(htmlBody) {
     return htmlBody;
 }
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+ sendResponse({dom: document.body.innerHTML});
+});
+
+// window.onload = onWindowLoad;
+
 function DOMtoString(selector) {
     if (selector) {
         selector = document.querySelector(selector);
