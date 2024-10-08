25246d245ed1df5678bbb27d64ec4b195d51c9ea
Bryan Kaperick <bryankaperick@gmail.com>
2024-10-08 15:44:41 +0200

    Doesnt work - we don't have access to page DOM from background script - make a content script

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,12 +2,12 @@ var languages = [];
 
 // Watch for changes to the user's options & apply them
 chrome.storage.onChanged.addListener((changes, area) => {
-  if (area === 'sync' && changes.options?.newValue) {
-    console.log(JSON.stringify(changes.options.newValue));
-    languages = changes.options.newValue.languages;
-    console.log("Updated languages: " + languages);
-  }
-});
+        if (area === 'sync' && changes.options?.newValue) {
+        console.log(JSON.stringify(changes.options.newValue));
+        languages = changes.options.newValue.languages;
+        console.log("Updated languages: " + languages);
+        }
+        });
 
 function DOMtoString(selector) {
     console.log("DOMtoString");
@@ -20,30 +20,39 @@ function DOMtoString(selector) {
     return selector.outerHTML;
 };
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-    const activeTab = chrome.tabs.query({ currentWindow: true, active: true });
-    var activeTabId = activeTab.id;
-//           return chrome.scripting.executeScript({
-//               target: { tabId: activeTabId },
-//               injectImmediately: true,  uncomment this to make it execute straight away, other wise it will wait for document_idle
-//               func: DOMtoString,
-//               args: ['body']  you can use this to target what element to get the html for
-//           });
-//       }).then(function (results) {
-//           message.innerText = results[0].result;
-//       }).catch(function (error) {
-//           message.innerText = 'There was an error injecting script : \n' + error.message;
-//       });
-// }
+        const activeTab = chrome.tabs.query({ currentWindow: true, active: true });
+        var activeTabId = activeTab.id;
+        //           return chrome.scripting.executeScript({
+        //               target: { tabId: activeTabId },
+        //               injectImmediately: true,  uncomment this to make it execute straight away, other wise it will wait for document_idle
+        //               func: DOMtoString,
+        //               args: ['body']  you can use this to target what element to get the html for
+        //           });
+        //       }).then(function (results) {
+        //           message.innerText = results[0].result;
+        //       }).catch(function (error) {
+        //           message.innerText = 'There was an error injecting script : \n' + error.message;
+        //       });
+        // }
 });
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-
-    if (changeInfo.status == 'complete' && tab.active && tab.url.includes("wikipedia.org")) {
+        if (changeInfo.status == 'complete' && tab.active && tab.url.includes("wikipedia.org")) {
+        var languageDom = document.getElementById("p-lang-label");
+        var links = [...languageDom.querySelectAll("a")];
+        var candidates = [];
+        links.forEach(l =>
+        {
+            if (languages.includes(l.lang)) {
+                candidates.push(l.lang);
+            }
+        });
         const re = /https:\/\/(\w\w)(\.wikipedia\.org\/wiki\/.*)/;
         var info = tab.url.match(re);
         var language = info[1];
         var article = info[2];
         
-        var other = languages.filter((x) => x !== language)[0];
+        // var other = languages.filter((x) => x !== language)[0];
+        var other = candidates[0];
         var redirect = "https://" + other + article;
         chrome.tabs.create({
             url: redirect
