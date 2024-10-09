8372ca4331fa83df81735b1309bbff4fb3eeb4d9
Bryan Kaperick <bryankaperick@gmail.com>
2024-10-09 16:49:48 +0200

    WIP working version of wikipedia api request

--- src/content.js
+++ src/content.js
@@ -14,29 +14,79 @@ chrome.storage.sync.get("options").then((l) => {
     // document.addEventListener("load", getLanguageRedirect(user_languages));
 });
 
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+            console.log(xmlHttp);
+            callback(xmlHttp.responseText);
+        }
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
 
 function getLanguageRedirect(languages) {
     
     console.log("page is loaded: " + document.URL + " with state " + document.readyState);
     console.log("doc: " + JSON.stringify(document.URL));
-    // var languageDom = document.querySelector("#p-lang-label");
-    // var languageDom = document.getElementById("p-lang-label");
+
+    // TODO get links via API languagelinks param instead
     var languageDom = document.getElementById("p-lang");
-    // var links_ = languageDom.value.querySelectorAll("p-lang");
-    // var links_ = languageDom.querySelectorAll("a#interlanguage-link-target");
     var links_ = languageDom.querySelectorAll("a.interlanguage-link-target");
     var links = [...links_];
-    var candidates = [];
+    var lang_to_url = {};
+    var lang_to_content_length = {};
     console.log(JSON.stringify(languageDom));
     console.log(links);
     links.forEach(l =>
     {
         if (languages.includes(l.lang)) {
-            candidates.push(l.lang);
+            lang_to_url[l.lang] = l.href;
         }
     });
     console.log("user languages loaded: " + languages);    
-    console.log("candidate languages: " + candidates);
+    //console.log("candidate languages: \n" + JSON.stringify(lang_to_url));
+    
+    const re = /https?:\/\/(www\.)?(.*)\.wikipedia\.org\/wiki\/([^#]*)#?(.*)?/;
+    var info = document.URL.match(re);
+    var base_url_language = info[2];
+    var title = info[3];
+    var section = info[4];
+    
+    for (const lang in lang_to_url) { 
+        // TODO this is the one we want, but fails due to CORS headers in wikipedia api response
+        // var wiki_api_raw_text = "https://" + lang + ".wikipedia.org/w/index.php?origin=*&title=" + title + "&action=raw&withCredentials=false"
+        // This one just gives us the english-language result
+        // var wiki_api_raw_text = "https://" + base_url_language + ".wikipedia.org/w/index.php?origin=*&title=" + title + "&action=raw&withCredentials=false"
+        /*
+        Tony_Hawk%27s_Underground:1 Access to XMLHttpRequest at 
+            'https://es.wikipedia.org/w/index.php?title=Tony_Hawk%27s_Underground&action=raw&origin=*&withCredentials=false' 
+            from origin 'https://en.wikipedia.org' 
+            has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+        */
+        // var wiki_api_raw_text = "https://" + lang + ".wikipedia.org/w/api.php?origin=*&title=" + title + "&text=" + title + "&action=parse"
+        // var wiki_api_raw_text = "https://" + lang + ".wikipedia.org/w/api.php?origin=*&titles=" + title + "&action=query"
+        var wiki_api_raw_text = "https://" + lang + ".wikipedia.org/w/api.php?origin=*&page=" + title + "&action=parse"
+        // https://fr.wikipedia.org/w/api.php?origin=*&page=Tony_Hawk%27s_Underground&action=parse 
+        console.log(lang + ": " + wiki_api_raw_text.length);
+        httpGetAsync(wiki_api_raw_text, response => lang_to_content_length[lang] = response.length);
+        
+        // $.ajax({ url: lang_to_url[lang], success: function(data) { alert(data); } });
+    }
+    console.log(lang_to_content_length);
+    var best_lang = undefined;
+    var max_len = -1;
+    for (const [lang,len] of Object.entries(lang_to_content_length)) {
+        if (len > max_len) {
+            max_len = len;
+            best_lang = lang;
+        }
+    }
+    var wiki_api_set_lang = "https://en.wikipedia.org/w/api.php?action=setpagelanguage&title=" + title + "&lang=" + best_lang + "&";
+    console.log(best_lang + ": " + wiki_api_set_lang);
+
 // window.onload = function() {
 //        if (changeInfo.status == 'complete' && tab.active && tab.url.includes("wikipedia.org")) {
 //            console.log("page is loaded: " + tab.url);
